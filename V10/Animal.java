0a252aa0ff83bfdd0f4c9847a65cf1eeb81118e9
ruihanjc <74535726+ruihanjc@users.noreply.github.com>
2021-02-25 18:14:03 +0000

    Add files via upload

---
+++ V10/Animal.java
@@ -0,0 +1,229 @@
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * A class representing shared characteristics of animals.
+ * 
+ * @author David J. Barnes and Michael Kölling
+ * @edited Chi Bene Chen and Rui Han Ji Chen
+ * @version 2016.02.29 (2)
+ */
+public abstract class Animal extends Actor{
+    private final boolean isFemale;
+
+    private boolean haveDisease;
+    
+    /**
+     * Create a new animal at location in field.
+     * 
+     * @param field The field currently occupied.
+     * @param location The location within the field.
+     * @param boolean Whether it has an random age or not
+     */
+    public Animal(boolean randomAge, Field field, Location location){
+        super(randomAge,field, location);
+        isFemale = getRandom().nextBoolean();
+        if(getRandom().nextDouble() <= getInfectionRate()){
+            haveDisease = true;
+        }
+    }
+
+    /**
+     * Make this animal act - that is: make it do
+     * whatever it wants/needs to do.
+     * @param newAnimals A list to receive newly born animals.
+     */
+    public abstract void act(List<Actor> newAnimals);
+    
+    /**
+     * Returns the infection rate of each animal
+     */
+    abstract protected double getInfectionRate();
+    
+    /**
+     * Returns the foodvalue for each animal
+     */
+    abstract protected int getFoodValue();
+
+    /**
+     * Returns the food level of each animal
+     */
+    abstract protected int getFoodLevel();
+    
+    /**
+     * Increments the hunger of each animal
+     */
+    abstract protected void incrementHunger();
+    
+    /**
+     * Adds the foodlevel of the animal
+     */        
+    abstract protected void addFoodLevel(int foodValue);
+    
+    /**
+     * Returns the gender of an animal
+     */
+    protected boolean getIsFemale(){
+        return isFemale;
+    }
+    
+    /**
+     * Returns if an animal has disease or not
+     */
+    protected boolean getHaveDisease(){
+        return haveDisease;
+    }
+
+    /**
+     * Sets if the animal has disease or not
+     */
+    protected void setHaveDisease(){
+        haveDisease = !haveDisease;
+    }
+
+    /**
+     * Multipy an animal depending of which type of animal is having the coitus.
+     * @param List<Actors> The list of actors in the field
+     * @param Class The class of animal that is breeding
+     */
+    protected void multiplyActor(List<Actor> actors, Class animalClass){
+        List<Location> free = getField().getFreeAdjacentLocations(getLocation());
+        int births = multiply(animalClass);
+        for(int b = 0; b < births && free.size() > 0; b++) {
+            Location loc = free.remove(0);
+            Actor young;
+            if(animalClass == Rabbit.class){
+                young = new Rabbit(false, getField(),loc);
+                actors.add(young);
+            }
+            if (animalClass == Fox.class){
+                young = new Fox(false, getField(),loc);
+                actors.add(young);
+            }
+            if (animalClass == Wolf.class){
+                young = new Wolf(false, getField(),loc);
+                actors.add(young);
+            }
+            if (animalClass == Snake.class){
+                young = new Snake(false, getField(),loc);
+                actors.add(young);
+            }
+            if (animalClass == Sheep.class){
+                young = new Sheep(false, getField(),loc);
+                actors.add(young);
+            }
+        }
+    }
+
+    /**
+     * Searching the food depending on the type of animal it is
+     * @parm Class preyClass
+     * @param String herbivore and carnivore
+     * @param int The foodvalue for each animal
+     */
+    protected Location findFood(Class preyClass, int foodValue, String eatingType) {
+        Field field = super.getField();
+        if (eatingType.equals("carnivore")) {
+            List<Location> adjacent = field.adjacentLocations(getLocation());
+            Iterator<Location> it = adjacent.iterator();
+            while (it.hasNext()) {
+                Location where = it.next();
+                Object actor = field.getObjectAt(where);
+                if (actor instanceof Rabbit && actor.getClass() == preyClass) {
+                    Rabbit rabbit = (Rabbit) actor;
+                    if (rabbit.isAlive()) {
+                        rabbit.setDead();
+                        addFoodLevel(getFoodValue());
+                        return where;
+                    }
+                }
+                if (actor instanceof Sheep && actor.getClass() == preyClass) {
+                    Sheep sheep = (Sheep) actor;
+                    if (sheep.isAlive()) {
+                        sheep.setDead();
+                        addFoodLevel(getFoodValue());
+                        return where;
+                    }
+                }
+            }
+        } else if (eatingType.equals("herbivore")) {
+            Location down = new Location(getLocation().getRow(), getLocation().getCol(), 0);
+            List<Location> adjacent = field.adjacentLocations(down);
+            Iterator<Location> it = adjacent.iterator();
+            while (it.hasNext()) {
+                Location where = it.next();
+                Object actor = field.getObjectAt(where);
+                if (actor instanceof Grass && actor.getClass() == preyClass) {
+                    Grass grass = (Grass) actor;
+                    if (grass.isAlive()) {
+                        grass.setDead();
+                        addFoodLevel(getFoodValue());
+                        return where;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+    
+    /**
+     * The number of births it will have
+     */
+    protected int multiply(Class animalType){
+        int births = 0;
+        if(canMultiply() && possibleToMultiply(animalType) && getRandom().nextDouble() <= getMultiplyProbability()) {
+            births = getRandom().nextInt(getMaxLitterSize()) + 1;
+        }
+        return births;
+    }
+    
+    /**
+     * Whether the animal is old enough to breed
+     */
+    protected boolean canMultiply(){
+        return (getAge() >= getMultiplyAge());
+    }
+    
+    /**
+     * Whether the animal is near an male gender animal of the same type
+     */
+    protected boolean possibleToMultiply(Class animalType){
+        for(Location location : getField().adjacentLocations(getLocation())){
+            Object locationObject = getField().getObjectAt(location);
+            if(locationObject != null && animalType.isInstance(locationObject)){
+                Animal a = (Animal)locationObject;
+                if (getIsFemale() && !a.getIsFemale()) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * Infecting the disease to other animals
+     */
+    public void infecting(){
+         Field field = super.getField();
+         List<Location> nearLocations = field.adjacentLocations(getLocation());
+         for(Location loc : nearLocations){
+             Object locationObject = field.getObjectAt(loc);
+             if(locationObject != null && locationObject instanceof Animal){
+                 Animal a = (Animal)locationObject;
+                 if(getRandom().nextDouble() <= a.getInfectionRate()) {
+                     setHaveDisease();
+                 }
+             }
+         }
+    }
+    
+    /**
+     * Killing the animals with disease if the condiction is true
+     */
+    public void killing(){
+        if(getAge() > getMaxAge()* 0.5){
+            setDead();
+         }
+    }
+}
---
+++ V10/Snake.java
@@ -0,0 +1,142 @@
+import java.util.List;
+
+/**
+ * A simple model of a snake.
+ * Snakes age, move, eat rabbits, and die.
+ * 
+ * @author Chi Bene Chen and Rui Han Ji Chen
+ * @version 2016.02.29 (2)
+ */
+public class Snake extends Animal{
+    private static final int MULTIPLY_AGE = 10;
+    // The age to which a snake can live.
+    private static final int MAX_AGE = 60;
+    // The likelihood of a snake breeding.
+    private static final double MULTIPLY_PROBABILITY = 0.1;
+    // The maximum number of births.
+    private static final int MAX_LITTER_SIZE = 4;
+    // The food value of a single rabbit. In effect, this is the
+    // number of steps a snake can go before it has to eat again.
+    private static final int FOOD_VALUE = 9;
+    // A shared random number generator to control breeding.    
+    private static final double INFECTION_RATE = 0.2;
+    // The food level of the animal
+    private int foodLevel;
+    
+    
+    public Snake(boolean randomAge, Field field, Location location)
+    {
+        super(randomAge,field, location);
+        if (randomAge) {
+            setAge(super.getRandom().nextInt(getMaxAge()));
+            foodLevel = super.getRandom().nextInt(45);
+        } else {
+            foodLevel = 45;
+        }
+    }
+    
+    /**
+     * This is what the snake does most of the time: it hunts for
+     * rabbits. In the process, it might breed, die of hunger,
+     * or die of old age.
+     * @param newSnakes A list to return newly born snakes.
+     */
+    @Override
+    public void act(List<Actor> newSnakes){
+        super.incrementAge();
+        incrementHunger();
+        if(isAlive()) {
+            super.multiplyActor(newSnakes, Snake.class);
+            // Move towards a source of food if found.
+            Location newLocation = findFood(Rabbit.class, FOOD_VALUE,"carnivore");
+        if(newLocation == null) {
+                newLocation = getField().freeAdjacentLocation(getLocation());
+        }
+        if(newLocation != null) {
+            super.setLocation(newLocation);
+        }else{
+            // Overcrowding.
+            super.setDead();
+        }
+        //if(getHaveDisease()){
+           //infecting();
+           //killing();
+        //}
+        }
+    }
+    
+    /**
+     * Returns the maximum age of each kind of actor
+     */
+    @Override
+    protected int getMaxAge(){
+        return MAX_AGE;
+    }
+
+    /**
+     * Returns the age the the animal can breed
+     */
+    @Override
+    protected int getMultiplyAge(){
+        return MULTIPLY_AGE;
+    }
+
+    /**
+     * Returns the maximum size that can breed 
+     */
+    @Override
+    protected int getMaxLitterSize(){
+        return MAX_LITTER_SIZE;
+    }
+
+    /**
+     * Returns the probability of breeding
+     */
+    @Override
+    protected double getMultiplyProbability(){
+        return MULTIPLY_PROBABILITY;
+    }
+    
+    /**
+     * Returns the infection rate
+     */
+    @Override
+    protected double getInfectionRate(){
+        return INFECTION_RATE;
+    }
+
+    /**
+     * Returns the food value
+     */
+    @Override
+    protected int getFoodValue(){
+        return FOOD_VALUE;
+    }
+
+    /**
+     * Returns the food level
+     */
+    @Override
+    protected int getFoodLevel(){
+        return foodLevel;
+    }
+    
+    /**
+     * Adds the foodlevel of the animal
+     */
+    @Override
+    protected void addFoodLevel(int foodValue){
+        foodLevel += FOOD_VALUE;
+    }
+    
+    /**
+     * Returns the maximum age of each kind of actor
+     */
+    @Override
+    protected void incrementHunger(){
+        foodLevel--;
+        if(foodLevel <= 0) {
+            setDead();
+        }
+    }
+}
